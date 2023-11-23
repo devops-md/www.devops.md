5a4faf70d494cd28fe4689e231880593c57ba6e7
Sebastian Zavadschi <sz@devops.md>
2023-11-23 17:25:08 +0000

    (Grav GitSync) Automatic Commit from Sebastian Zavadschi

--- plugins/gantry5/compat/vendor/composer/installed.php
+++ plugins/gantry5/compat/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'gantry/grav-compat',
-        'pretty_version' => '5.5.15',
-        'version' => '5.5.15.0',
-        'reference' => '3a0fd8bd20a5d507fe8a5792d33f414e8730aea6',
+        'pretty_version' => '5.5.17',
+        'version' => '5.5.17.0',
+        'reference' => 'f8253982d1adf7200218cbb49f4364db047b235c',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,18 +11,18 @@
     ),
     'versions' => array(
         'gantry/grav-compat' => array(
-            'pretty_version' => '5.5.15',
-            'version' => '5.5.15.0',
-            'reference' => '3a0fd8bd20a5d507fe8a5792d33f414e8730aea6',
+            'pretty_version' => '5.5.17',
+            'version' => '5.5.17.0',
+            'reference' => 'f8253982d1adf7200218cbb49f4364db047b235c',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
             'dev_requirement' => false,
         ),
         'scssphp/scssphp' => array(
-            'pretty_version' => 'v1.10.3',
-            'version' => '1.10.3.0',
-            'reference' => '0f1e1516ed2412ad43e42a6a319e77624ba1f713',
+            'pretty_version' => 'v1.11.1',
+            'version' => '1.11.1.0',
+            'reference' => 'ace2503684bab0dcc817d7614c8a54b865122414',
             'type' => 'library',
             'install_path' => __DIR__ . '/../scssphp/scssphp',
             'aliases' => array(),
